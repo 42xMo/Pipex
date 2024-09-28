96f44c94cfa78562b356274b34d9c1ed406538b9
Mohamed Abdessmad <mabdessm@c4r1p1.42nice.fr>
2024-09-29 00:09:14 +0200

    makefile changes

--- mandatory/pipex.c
+++ mandatory/pipex.c
@@ -6,7 +6,7 @@
 /*   By: mabdessm <mabdessm@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/09 00:33:29 by mabdessm          #+#    #+#             */
-/*   Updated: 2024/09/28 21:55:45 by mabdessm         ###   ########.fr       */
+/*   Updated: 2024/09/28 22:17:25 by mabdessm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,7 +42,7 @@ int	main(int argc, char **argv, char **envp)
 	t_pipex	pipex;
 	int		i;
 
-	if (argc >= 5)
+	if (argc == 5)
 	{
 		assign_pipex(&pipex, argv, argc, envp);
 		if (!pipex.cmd_not_found)
